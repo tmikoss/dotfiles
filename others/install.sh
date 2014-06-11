593be19df326be7c5272b4aa512add06f4686d85
Toms Mikoss <toms.mikoss@gmail.com>
2014-06-11 09:05:06 +0300

    Slate

--- others/install.sh
+++ others/install.sh
@@ -11,3 +11,6 @@ mkdir -p ~/Library/Application\ Support/Pow/Hosts
 ln -s ~/Library/Application\ Support/Pow/Hosts ~/.pow
 sudo pow --install-system
 pow --install-local
+
+# Slate config
+ln -sf ~/.dotfiles/others/.slate ~/.slate
