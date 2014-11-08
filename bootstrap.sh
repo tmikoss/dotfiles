6534c7460fc5e11cdbfc453844833c31b15b2f15
Toms Mikoss <toms.mikoss@gmail.com>
2014-11-08 18:24:25 +0200

    Updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,55 @@
 #!/usr/bin/env bash
 
 echo 'Installing Homebrew'
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew bundle ~/.dotfiles/Brewfile
+brew update
+
+brew upgrade
+
+brew install git
+brew install imagemagick
+brew install redis
+brew install postgresql
+brew install postgis
+brew install mongodb
+brew install memcached
+brew install node
+brew install phantomjs
+brew install heroku-toolbelt
+brew install rbenv
+brew install ruby-build
+brew install pow
+brew install rbenv-default-gems
+brew install rbenv-gemset
+brew install rbenv-binstubs
+brew install rbenv-gem-rehash
+brew install zsh-completions
+brew install ssh-copy-id
+brew install mysql
+brew install python
+brew install freetds
+
+brew tap caskroom/cask
+brew install brew-cask
+
+brew cask install --force dropbox
+brew cask install --force google-chrome
+brew cask install --force vlc
+brew cask install --force virtualbox
+brew cask install --force github
+brew cask install --force skype
+brew cask install --force atom
+brew cask install --force slate
+brew cask install --force skitch
+brew cask install --force evernote
+brew cask install --force firefox
+brew cask install --force toad
+brew cask install --force sequel-pro
+brew cask install --force hipchat
+
+brew cleanup
+
 brew bundle ~/.dotfiles/Caskfile
 
 find ~/.dotfiles -maxdepth 2 -name 'install.sh' | while read installer ; do sh -c "${installer}" ; done
