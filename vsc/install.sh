d44d0f204c52c785a1750b9a92f5bee398b42527
Toms Mikoss <toms.mikoss@gmail.com>
2018-10-08 19:20:58 +0300

    vsc keybinds

--- vsc/install.sh
+++ vsc/install.sh
@@ -3,3 +3,4 @@
 echo 'Installing VSC'
 
 ln -sf ~/.dotfiles/vsc/settings.json  ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/.dotfiles/vsc/keybindings.json  ~/Library/Application\ Support/Code/User/keybindings.json
